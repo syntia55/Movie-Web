69230f14e6a4cd45a7dfab016e1d17931859cfce
syntia55 <49128705+syntia55@users.noreply.github.com>
2021-06-28 09:50:47 +0700

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import "regenerator-runtime";
+import "./css/style.css";
+import "./script/component/app-bar.js";
+import "./script/component/footer.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
